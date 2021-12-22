2be410da4caa0f8aa4a4fae75e04f16c70c652e4
Dan Possehl <danielbpossehl@gmail.com>
2021-12-22 13:57:10 -0700

    core section finished

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1304dd9a4456ebf1c304";
+/******/ 	var hotCurrentHash = "227d3e22637fd2cba436";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
