233e5fb56b856f6d134d916f786690de5ebbe6fe
Dan Possehl <danielbpossehl@gmail.com>
2021-12-23 12:04:12 -0700

    testimonial section complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d9107a306ebab2a5e703";
+/******/ 	var hotCurrentHash = "57630da1caffcaca370c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
