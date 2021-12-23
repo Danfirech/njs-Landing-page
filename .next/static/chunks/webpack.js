44b53daf7188b2473d78ba1e711db0c7d6d04027
Dan Possehl <danielbpossehl@gmail.com>
2021-12-23 10:32:27 -0700

    Price Card taking shape

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f3e5c1c5ccacb5ddfa60";
+/******/ 	var hotCurrentHash = "c12cf5c890502d130e71";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
